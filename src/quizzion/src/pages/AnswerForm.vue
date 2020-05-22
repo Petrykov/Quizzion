28fa5ae824b3b27dfdc9a59d8539cad245487a02
victorhubbers <469487@student.saxion.nl>
2020-05-22 19:52:13 +0200

    Answer page pretty close to mockups Not componentized

--- src/quizzion/src/pages/AnswerForm.vue
+++ src/quizzion/src/pages/AnswerForm.vue
@@ -1,19 +1,25 @@
 <template>
   <q-page>
-    <div class="row window-height items-center">
+    <div class="bg-image row window-height items-center">
       <div class="left-half col-xs-12 col-sm-6">
-        <h3>{{ currentQuestion.title}}</h3>
-          <h5> {{currentQuestion.description}} </h5>
+        <h3 class="xs-hide">{{ currentQuestion.title}}</h3>
+        <h5 class="xs">{{ currentQuestion.title}}</h5>
+          <h5 class="xs-hide"> {{currentQuestion.description}} </h5>
+          <p class="xs"> {{currentQuestion.description}} </p>
           <img
             alt="Quasar logo"
             src="~assets/quasar-logo-full.svg"
           >
       </div>
         <div class="right-half col-xs-12 col-sm-6">
-          <p>A: Quasar</p>
+          <div class="counter-container">
+            <h4 class="xs-hide counter">15</h4>
+            <h5 class="xs counter">15</h5>
+          </div>
+          <p style="margin-top: 5%">A: Quasar</p>
           <p>B: JavaScript</p>
           <p>C: VueJS</p>
-          <p>D: C++</p>
+          <p style="margin-bottom: 16%">D: C++</p>
         </div>
       </div>
   </q-page>
@@ -37,31 +43,70 @@
 <style scoped>
   * { padding: 0; margin: 0; }
 
-  .left-half{
+  .counter-container {
+    transform: translateY(-50%);
+    border-radius: 100%;
+    margin: auto;
+    height: fit-content;
+    width: fit-content;
+    background-color: #ffdcb4;
+  }
 
+  .counter {
+    line-height: 70px;
+    width: 70px
+  }
+
+  .bg-image{
+    background-image: url("~assets/bg_answer_screen.png");
+  }
+
+  .left-half {
+    margin-bottom: 10%;
+    margin-top: 5%;
+    padding-left: 5%;
+    padding-right: 5%;
   }
 
   .right-half {
-    padding-top: 10%;
-    padding-bottom: 10%;
     background: #522785;
-    border-radius: 15px;
+    border-radius: 25px;
+    border-right: 5px solid white;
     text-align: center;
   }
 
   img {
     display: block;
+    margin-top: 5%;
     margin-left: auto;
     margin-right: auto;
   }
 
-  p {
-    padding: 3%;
+  .right-half p {
     background: white;
+    display: inline-block;
+    border-radius: 25px;
+    cursor: pointer;
+    margin-top: 3%;
+    margin-bottom: 3%;
     width: 85%;
-    display: inline-table;
-    margin: 2%;
-    border-radius: 5px;
-    /*text-align: left;*/
+    line-height: 75px;
+    text-align: center;
+    font-size: large;
+  }
+
+  @media screen and (max-width: 600px) {
+    .right-half {
+      border-right: 0;
+    }
+
+    .right-half p {
+      line-height: 50px;
+    }
+
+    .counter {
+      line-height: 50px;
+      width: 50px
+    }
   }
 </style>
