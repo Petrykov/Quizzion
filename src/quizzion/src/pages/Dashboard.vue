786ead37077d8256d5ada4d717e5ba6ed2880b27
Kasia472064 <472064@student.saxion.nl>
2020-05-23 14:17:51 +0200

    basic functionality

--- src/quizzion/src/pages/Dashboard.vue
+++ src/quizzion/src/pages/Dashboard.vue
@@ -1,3 +1,4 @@
+
 <template>
     <div>
              <div class="split left">
@@ -5,40 +6,59 @@
                 <h5 class="text-left"> Welcome back to the gamespace, we missed you! </h5>
                 <h5 class="text-left" > Quizzes </h5>
 
-                <q-btn class="quizzes button"
-                         color="red" 
-                         
-                > </q-btn>
-
+                <q-btn  v-for="(quiz, index) in quizzes" :key="quiz.id" class="quizzes button"
+                       :color = quizzes[index].color
+                       @click="getIndex(index)"
+                ></q-btn>
+               
                 <q-btn class="new quiz button"
                  icon="fas fa-plus"
                  color="grey"
-                
+                   to="/add" clickable
                 ></q-btn>
 
         </div>
-        <div class="split right" >
-            <i class="fas fa-edit"></i>
-            <h3 class="text-right">Quiz name</h3>
-            <p class="text-right" >  Quiz description: Lorem ipsum dolor sit amet, 
-                consectetur  adipiscing elit. Nunc rutrum auctor neque ut  </p> 
+        
+        <div v-if="pos > -1"  class="split right" 
+        v-bind:style="{ backgroundColor: quizzes[pos].color}">
+
+            <i class="fas fa-edit" to="/edit" clickable ></i>
+          
+            <h3 class="text-right">{{quizzes[pos].name}}</h3>
+
+            <p class="text-right" >  {{quizzes[pos].description}}  </p> 
+
             <span style="margin-left: 5%">
                  <tr>
                      <th>
                          <p class="text-right" style="font-size: 15px, margin-left: 140px" >Questions </p>   
                      </th>
                      <th>
+                         <div to="/edit" clickable>
                           <i class="fas fa-edit" style="font-size: 15px "></i>
+                         </div>
                      </th> 
                  </tr>
             </span>
-                 
-                <hr class="text-right">
 
-                <p class="text-right" style="padding-top: 20px"> question title 1 </p>
-                <p class="text-right" style="padding-top: 20px"> question title 1 </p>
-              <q-btn id="start_quiz_button" unelevated rounded color="white" text-color="black" label="Start quiz" />
-               
+            <hr class="text-right">
+
+                   <!-- //TODO loop for all questions in quiz -->
+
+                   <ul>
+                    <li v-for="question in quizzes" :key="question">
+                    <ul>
+                    <li v-for="q in question.questions" :key="q.id">
+                        <p class="text-right" style="padding-top: 10px">   {{ q }} </p>
+                      
+                     </li>
+                    </ul>
+                    </li>
+                </ul>
+            
+                <div >
+                     <q-btn to="/add" clickable id="start_quiz_button" unelevated rounded color="white" text-color="black" label="Start quiz"  />
+                </div>
 
              </div>
     </div>
@@ -49,13 +69,44 @@
 export default {
     data (){
         return {
-            
+            quizzes: [
+                {
+                    id: 0,
+                    name: 'Pub quiz 0',
+                    description: ' Description quiz 0 Lorem ipsum dolor sit amet, consectetur  adipiscing elit. Nunc rutrum auctor neque ut',
+                    questions: [ 'question 1'],
+                    color: 'red'
+                },
+                 {
+                     id: 1,
+                    name: 'Pub quiz 1',
+                    description: 'Description quiz 1 Lorem ipsum dolor sit amet, consectetur  adipiscing elit. Nunc rutrum auctor neque ut',
+                     questions: ['question 1'],
+                    color: 'blue'
+                },
+                 {  
+                     id: 2,
+                    name: 'Pub quiz 2',
+                    description: 'Description quiz 2 zzzzz Lorem ipsum dolor sit amet, consectetur  adipiscing elit. Nunc rutrum auctor neque ut',
+                     questions: ['question 1'],
+                    color: 'green'
+                }
+            ],
+            pos: 0
         }
-    }
+    },
+    methods: {
+        getIndex(position) {
+            console.log(position);
+            return this.pos=position;
+        }
+    } 
+    
 }
 </script>
 
 <style>
+
 
 .split {
   height: 100%;
@@ -64,13 +115,13 @@ export default {
   z-index: 1; 
   top: 0;
   padding-top: 20px;
+  
 }
 
 .right {
   right: 0;
   margin: 1%;
   border-radius: 25px;
-  background-color: blueviolet;
 }
 .text-left{
     text-decoration-color: black;
@@ -86,6 +137,7 @@ export default {
   text-decoration: none;
   display: inline-block;
   margin: 7%;
+ 
 }
 
 #start_quiz_button{ 
@@ -108,7 +160,5 @@ export default {
     margin-right: 5%;
     text-align: left;
 }
-
-
 
 </style>
