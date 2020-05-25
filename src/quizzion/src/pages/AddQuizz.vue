3ea40bb705bf4b321620ab51c02fc42e0938dbea
Mark Kravchuk <460888@student.saxion.nl>
2020-05-25 17:41:25 +0200

    Added timer functionality

--- src/quizzion/src/pages/AddQuizz.vue
+++ src/quizzion/src/pages/AddQuizz.vue
@@ -7,49 +7,41 @@
         <div class="vertical-allignment" style="">
 
           <q-scroll-area
-                class ="scroll-area"
-                :thumb-style="thumbStyle"
-                :content-style="contentStyle"
-                :content-active-style="contentActiveStyle"
-                style="height: 200px; max-width: 300px;">
+            class="scroll-area"
+            style="height: 275px; max-width: 300px;">
+<!--            :thumb-style="thumbStyle"-->
+<!--            :content-style="contentStyle"-->
+<!--            :content-active-style="contentActiveStyle"-->
+
 
             <p v-for="(btn,id) in questions" :key="id">
               <q-btn
-                :outline="id == selectedQuestion ? false : true"
+                :outline="id!=selectedQuestion"
                 rounded color="black"
-                @click="onQustionClick(id)">
-                  {{btn.name}}
+                @click="onQuestionClick(id)">
+                {{btn.name}}
               </q-btn>
-              
-              <q-icon 
-                name = "remove_circle_outline"
-                color= "black"
-                style = "cursor : pointer;"
-                size = "2em"
-                class = "q-ml-md q-mb-xs"
-                @click = "deleteQuestion(id)"/>
             </p>
 
           </q-scroll-area>
-          
-          <q-icon 
-                name = "add_circle_outline"
-                color= "green-7"
-                style = "cursor : pointer;"
-                size = "3em"
-                class = "q-mt-md"
-                @click = "addNewQuestion"/>
-              
+
+          <q-icon
+            name="add_circle_outline"
+            color="green-7"
+            style="cursor : pointer;"
+            size="3em"
+            class="q-mt-md"
+            @click="addNewQuestion"/>
+
 
         </div>
       </q-page-container>
 
-      <q-page-container
-        v-if="questions.length"
-        class="col q-pa-lg" style="background: #181c30; border-radius: 2em;">
+      <q-page-container class="col q-pa-lg" style="background: #181c30; border-radius: 2em;">
         <q-page padding>
 
-          <q-input class="questionInput" dark v-model="questions[selectedQuestion].name" color="grey-12" label="Question's title"
+          <q-input class="questionInput" dark v-model="questions[selectedQuestion].name" color="grey-12"
+                   label="Question's title"
                    label-color="grey"/>
 
           <q-input
@@ -65,81 +57,68 @@
           <p class="paragraph" style="color:white; font-size:2em;">The answers?</p>
 
           <div class="col">
-            <div class="row" v-for="(answer, index) in questions[selectedQuestion].answers" :key="answer.id">
+            <div class="row" v-for="answer in questions[selectedQuestion].answers" :key="answer.id">
               <q-checkbox dark v-model="answer.isCorrect"/>
-              <q-input  class="answer" 
-                        dense
-                        style="color:grey;"   
-                        dark 
-                        v-model="answer.name">
+              <q-input class="answer"
+                       dense
+                       style="color:grey;"
+                       dark
+                       v-model="answer.name">
+
               </q-input>
-
-              <q-icon 
-                name = "clear"
-                color= "red-7"
-                class = "q-mt-md"
-                style = "cursor : pointer;"
-                size = "2em"
-                @click="deleteAnswer(index)"/>
-
             </div>
 
-              <form>
-                <div class="row insert_new">
-                   
-                  <q-input 
-                    class="addQuestionTxt" 
-                    style="color:grey;" 
-                    label="Add new answer" 
-                    v-model="newAnswer"
-                    :rules="[val => !!val || 'Field is required']"
-                    dark/>
+            <div class="row insert_new">
+              <q-icon
+                name="add_circle_outline"
+                color="green-7"
+                style="cursor : pointer;"
+                size="2em"
+                class="addQuestionBtn"
+                @click="addAnswer"/>
 
-                  <q-icon 
-                    v-if = "newAnswer.length !== 0"
-                    name = "add_circle_outline"
-                    color= "green-7"
-                    style = "cursor : pointer;"
-                    size = "2em"
-                    class="addQuestionBtn"
-                    @click="addAnswer(id)"/>
-
-                  </div>
-                </form>
+              <q-input class="addQuestionTxt" style="color:grey;" dense label="Add new answer" v-model="newAnswer"
+                       dark></q-input>
+            </div>
           </div>
 
           <p class="paragraph" style="color:white; font-size:2em;">What about timer?</p>
 
-        <div class="row"> 
-          <q-icon 
-              name = "timer"
-              color= "white"
-              style = "cursor : pointer;"
-              size = "5em"
-              class="q-mr-xs"/>
+          <div class="row">
 
-              <q-input dark class="q-ml-lg" v-model="questions[selectedQuestion].time.value" label="Set a time for a question">
+            <q-icon
+              name="timer"
+              color="white"
+              style=""
+              size="5em"
+              class="q-mr-xs q-mt-md"/>
 
-              </q-input>
+            <div class="row col">
 
-              <q-btn-dropdown size="10px" style="padding-left: 0.5em; font-size: 15px;" dense class="q-ml-lg" color="primary" :label="questions[selectedQuestion].time.unit">
-                <q-list>
-                  <q-item clickable v-close-popup
-                  @click="changeUnit('Minutes')">
-                    <q-item-section>
-                      <q-item-label>Minutes</q-item-label>
-                    </q-item-section>
-                  </q-item>
+              <q-btn v-for="(time, index) in ['5 sec', '10 sec', '15 sec', '30 sec', '1 min']"
+                     v-bind:key="index"
+                     rounded
+                     dark
+                     :outline="questions[selectedQuestion].time === time ? false : true"
+                     :text-color="questions[selectedQuestion].time === time ? 'black' : 'white'"
+                     :color="questions[selectedQuestion].time === time ? 'white' : 'none'"
+                     class="col q-ml-md q-mr-md q-mt-lg q-mb-lg"
+                     size="12px"
+                     @click="changeTime(time)"
+                     :label="time"/>
 
-                  <q-item clickable v-close-popup
-                  @click="changeUnit('Seconds')">
-                    <q-item-section>
-                      <q-item-label>Seconds</q-item-label>
-                    </q-item-section>
-                  </q-item>
+              <q-btn
+                     rounded
+                     dark
+                     :outline="questions[selectedQuestion].time === 'infinity' ? false : true"
+                     :text-color="questions[selectedQuestion].time === 'infinity' ? 'black' : 'white'"
+                     :color="questions[selectedQuestion].time === 'infinity' ? 'white' : 'none'"
+                     @click="changeTime('infinity')"
+                     class="col q-ml-md q-mr-md q-mt-lg q-mb-lg"
+                     size="12px"
+                     icon="all_inclusive"/>
 
-                </q-list>
-              </q-btn-dropdown>
+            </div>
           </div>
 
         </q-page>
@@ -163,11 +142,8 @@
                         id: 1,
                         name: 'Question title 1',
                         description: 'Description for the first question',
-                        time: {
-                          unit: 'minutes',
-                          value: 0
-                        },
-
+                        active: true,
+                        time: '5 sec',
                         answers: [
                             {
                                 id: 1,
@@ -189,9 +165,10 @@
                         id: 2,
                         name: 'Question title 2',
                         description: 'Description for the second question',
+                        active: false,
                         time: {
-                          unit: 'minutes',
-                          value: 0
+                            unit: 'minutes',
+                            value: 0
                         },
 
                         answers: [
@@ -215,9 +192,10 @@
                         id: 3,
                         name: 'Question title 3',
                         description: 'Description for third question',
+                        active: false,
                         time: {
-                          unit: 'minutes',
-                          value: 0
+                            unit: 'minutes',
+                            value: 0
                         },
 
                         answers: [
@@ -243,58 +221,43 @@
         },
 
         methods: {
-            onQustionClick(id) {
+            onQuestionClick(id) {
                 this.selectedQuestion = id;
             },
 
-            addAnswer(){
-              let q = this.questions[this.selectedQuestion].answers;
-
-                q.push({
-                  id: q[q.length - 1] + 1,
-                  name: this.newAnswer,
-                  isCorrect: false,
-                });
-
-              this.newAnswer = '';
+            changeTime(time) {
+                this.questions[this.selectedQuestion].time = time;
             },
 
-            changeUnit(type){
+            addAnswer() {
+                let q = this.questions[this.selectedQuestion].answers;
+                q.push({
+                    id: q[q.length - 1] + 1,
+                    name: this.newAnswer,
+                    isCorrect: false,
+                });
+
+                this.newAnswer = '';
+            },
+
+            changeUnit(type) {
                 console.log('inside');
                 this.questions[this.selectedQuestion].time.unit = type;
             },
 
-            addNewQuestion(){
+            addNewQuestion() {
+                this.questions.push({
+                    id: this.questions[this.selectedQuestion].answers[this.questions[this.selectedQuestion].answers.length - 1] + 1,
+                    name: 'New question',
+                    description: '',
+                    active: false,
+                    time: {
+                        unit: 'seconds',
+                        value: 0
+                    },
 
-              let questionId; 
-
-              if(this.questions.length === 0){
-                questionId = 0
-              }else{
-                questionId = this.questions[this.selectedQuestion].answers[this.questions[this.selectedQuestion].answers.length-1]+1
-              }
-
-              this.questions.push({
-                        id: questionId,
-                        name: 'New question',
-                        description: '',
-                        time: {
-                          unit: 'seconds',
-                          value: 0
-                        },
-
-                        answers: [
-                        ]
-                    })
-            },
-
-            deleteAnswer(index){
-              this.questions[this.selectedQuestion].answers.splice(index, 1);
-            },
-
-            deleteQuestion(index){
-              // console.log('inside: ' + index);
-              this.questions.splice(index, 1);
+                    answers: []
+                })
             }
         }
     }
@@ -313,10 +276,10 @@
     transform: translateY(-50%);
   }
 
-  .scroll-area{
-    border: 1px solid black;
-    border-radius: 5px;
-    background: #d1d1e0;
+  .scroll-area {
+    border: 0.12em solid #d8d8d8;
+    border-radius: 8px;
+    /*background: #d1d1e0;*/
     margin: 0 auto;
   }
 
@@ -329,30 +292,30 @@
     margin-top: 2em;
   }
 
-  .editBtn{
+  .editBtn {
     margin-top: 2em;
     margin-left: 1em;
   }
 
-  .answer{
+  .answer {
     margin-left: 1em;
     /* margin-top: 1em; */
   }
 
-  .addQuestionBtn{
+  .addQuestionBtn {
     margin-top: 0.5em;
     margin-left: 0.25em;
   }
 
-  .addQuestionTxt{
-    margin-left: 3.7em;
+  .addQuestionTxt {
+    margin-left: 1.2em;
   }
 
   .q-checkbox__bg {
     border-radius: 10px;
   }
 
-  .insert_new{
+  .insert_new {
     margin-top: 1em;
   }
 
