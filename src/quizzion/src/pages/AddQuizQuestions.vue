bf86501a8e72393b99e11eee6d31b8a625dfff1d
Vladyslav <461157@student.saxion.nl>
2020-05-28 15:02:06 +0200

    all changes

--- src/quizzion/src/pages/AddQuizQuestions.vue
+++ src/quizzion/src/pages/AddQuizQuestions.vue
@@ -2,7 +2,9 @@ l<template>
   <q-page container style="" class="shadow-2 rounded-borders">
     <div class="row q-pa-lg" style="">
       <q-page-container class="col q-pa-lg" style="text-align: center;">
-        <p class = "q-mt-md" style="font-size:3em;">Quiz name</p>
+        <p class = "q-mt-md" style="font-size:3em;">
+          {{ currentQuiz.title }}
+          </p>
         <div class="vertical-allignment" style="">
           <q-scroll-area
             class="scroll-area"
