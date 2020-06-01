ed4d7d355eba6f878385809d0117d6e5e782fe9d
tien <460592@student.saxion.nl>
2020-06-01 16:26:56 +0200

    user logout

--- src/backend/apidoc/api_data.js
+++ src/backend/apidoc/api_data.js
@@ -2,8 +2,38 @@ define({ "api": [
   {
     "type": "post",
     "url": "/user/login",
-    "title": "To login to the server",
+    "title": "Login access for customer",
     "name": "Login",
+    "group": "User",
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "username",
+            "description": "<p>Username of the User.</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "password",
+            "description": "<p>Password of the User.</p>"
+          }
+        ]
+      }
+    },
+    "version": "0.0.0",
+    "filename": "routers/users.js",
+    "groupTitle": "User"
+  },
+  {
+    "type": "delete",
+    "url": "/user/logout",
+    "title": "Logout access for customer",
+    "name": "Logout",
     "group": "User",
     "success": {
       "fields": {
--- src/backend/routers/users.js
+++ src/backend/routers/users.js
@@ -3,26 +3,28 @@
 let router = module.exports = require('express').Router();
 const md5 = require('md5');
 const axios = require('axios');
+var authenticateUser;
+var token;
+var uid;
+const baseUrl="https://lab.dev.easion.nl/backend/api/v5"
 
 /**
- * @api {post} /user/login To login to the server
+ * @api {post} /user/login Login access for customer
  * @apiName Login
  * @apiGroup User
 *
  * @apiSuccess {String} username Username of the User.
  * @apiSuccess {String} password  Password of the User.
+ * @apiError UserNotFound The <code>id</code> of the User was not found.
  */
 
 router.post('/login', (req, rsp) => {
-    var token;
-    var uid;
-    var authenticateUser;
 
     if (req.body.username == undefined) { rsp.status(500).json({ error: "Missing username" }) };
     if (req.body.password == undefined) { rsp.status(500).json({ error: "Missing password" }) };
 
     var hashPassword = md5(req.body.password)
-    axios.post('https://lab.dev.easion.nl/backend/api/v5/account/access', {
+    axios.post(`${baseUrl}/account/access`, {
         username: req.body.username,
         password: hashPassword
     })
@@ -31,12 +33,30 @@ router.post('/login', (req, rsp) => {
             uid = res.data.authenticate.uid
             axios.defaults.headers.common['X-CSRFToken'] = token;
             axios.defaults.headers.common['X-Database'] = 'lab';
-            axios.get(`https://lab.dev.easion.nl/backend/api/v5/user/${uid}`)
+            axios.get(`${baseUrl}/user/${uid}`)
                 .then( function(res){
                     authenticateUser = res.data.user
                     rsp.status(200).send(authenticateUser)
                 })
-                .catch(error => console.log(error))
+                .catch(error => rsp.status(500).send(error))
         })
-        .catch(error => console.log(error));
+        .catch(error => rsp.status(500).send(error));
 });
+/**
+ * @api {delete} /user/logout Logout access for customer
+ * @apiName Logout
+ * @apiGroup User
+*
+ * @apiSuccess {String} username Username of the User.
+ * @apiSuccess {String} password  Password of the User.
+ */
+
+router.delete('/logout', (req, rsp) => {
+    if(authenticateUser==null){
+        rsp.status(400).send("no user logged in")
+    }
+    axios.defaults.headers.common['X-CSRFToken'] = token;
+    axios.delete(`${baseUrl}/account/access`)
+    .then(res => rsp.status(200).send(res.data.messages))
+    .catch(error => rsp.status(500).send(error))
+})
