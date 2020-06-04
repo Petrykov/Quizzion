8b36424dc29b7c5f494920f57628aeea6ea4a3dc
Kasia472064 <472064@student.saxion.nl>
2020-06-04 18:50:10 +0200

    form GET POST

--- src/backend/routers/quizzes.js
+++ src/backend/routers/quizzes.js
@@ -1,7 +1,7 @@
 let router = module.exports = require('express').Router();
 const axios = require('axios').default;
-const token = '6b1fb098df45c37f5e8178654f5065b2f30ea0da5642deb685067a30f04fd557010f7b2c95cce110035ff4f3011838072d3bd0719bf21c786dfb75c43b36e24f'
-axios.defaults.headers.common['X-CSRFToken'] = '6b1fb098df45c37f5e8178654f5065b2f30ea0da5642deb685067a30f04fd557010f7b2c95cce110035ff4f3011838072d3bd0719bf21c786dfb75c43b36e24f';
+const token = 'edf4a556f48b23ca4b76fb2eb6e8e56c1fb2209fc38d43bd4673660d32a9e84c8689b3181eb54cdcffe47f7c9d4146976314b7dac7ddfba92414715706ffc5f1'
+axios.defaults.headers.common['X-CSRFToken'] = 'edf4a556f48b23ca4b76fb2eb6e8e56c1fb2209fc38d43bd4673660d32a9e84c8689b3181eb54cdcffe47f7c9d4146976314b7dac7ddfba92414715706ffc5f1';
 axios.defaults.headers.common['X-Database'] = 'lab';
 axios.defaults.headers.common['Content-Type'] = 'application/json';
 const baseUrl = "https://lab.dev.easion.nl/backend/api/v5";
@@ -83,7 +83,6 @@ router.post('/', (req, rsp) => {
 
 
     // to get from user input
-
     req.body.active = true;
     req.body.color= 'blue';
     req.body.logo = 'http';
@@ -116,7 +115,7 @@ router.post('/', (req, rsp) => {
 
 
 //edit quiz
-router.put('/:quiz.id', (req, rsp) => {
+router.put('/:quizId', (req, rsp) => {
 
    let active = req.body.active;
    let color = req.body.color;
@@ -143,6 +142,61 @@ router.put('/:quiz.id', (req, rsp) => {
 
 });
 
+// create new form
+
+router.post('/start', (req, rsp) => {
+    axios.post(`${baseUrl}/form`, {
+        frm_label: 'Some label',
+        type: 'survey',
+        uh: "user hash list",
+        th: req.body.th,
+        field:{
+            frm_date_start: new Date().getFullYear 
+        }
+    }).then((response) => {
+        rsp.status(201).json(response.data);
+    }).catch((err) => {
+        console.log("error");
+        rsp.status(400).json(err);
+    });
+});
 
 
+// get existing form
+
+router.get('/start', (req, rsp) => {
+
+    axios.get(baseUrl+`/form`, {
+        headers: {
+            'X-CSRFToken': token,
+            'X-Database': 'lab',
+            'Content-Type': 'application/json'
+        }
+
+    }).then(function (response) {
+
+        rsp.send(response.data)
+
+    }).catch(error => rsp.send(error))
+
+})
+
+//get particular form (by fh)
+
+router.get('/start/:formId', (req, rsp) => {
+
+    axios.get(`${baseUrl}/form/${req.params.formId}`, {
+        headers: {
+            'X-CSRFToken': token,
+            'X-Database': 'lab',
+            'Content-Type': 'application/json'
+        }
+
+    }).then(function (response) {
+
+        rsp.send(response.data)
+
+    }).catch(error => rsp.send(error))
+
+})
 
