0e0ee44218dcbabcc21b8f8b02f01dc64608ac28
Vladyslav <461157@student.saxion.nl>
2020-06-04 14:54:09 +0200

    dashboard page

--- src/quizzion/src/components/DashboardRight.vue
+++ src/quizzion/src/components/DashboardRight.vue
@@ -15,95 +15,114 @@
 
     <div style="height: fit-content;">
       <div class="row">
-        <h3 style="color: white; border-bottom: 1px solid black; width: fit-content;" class="col">{{ currentQuiz.title }}</h3>
+        <h3 style="color: white; width: fit-content;" class="col reduce">{{ currentQuiz.title }}</h3>
         <q-icon @click="editQuiz" name="edit" size="2em" style="cursor : pointer;" color="white"/>
       </div>
-      <p style="color: black; font-size: 1.5em; border-bottom: 1px solid white; width: fit-content;">{{ currentQuiz.description }}</p>
+      <p style="color: black; font-size: 1.5em; width: fit-content;">{{currentQuiz.description }}</p>
     </div>
 
-    <div class="col-5">
-      <div class="row justify-between">
-        <p style="color:white; font-size: 2em;">Questions</p>
+    <div style="display: inline-block">
+      <div class="col-5">
+        <div class="row justify-between">
+          <p style="color:white; font-size: 2em;">Questions</p>
 
-        <div>
-          <q-btn
-            unelevated
-            dark
-            dense
-            style="background: transparent;"
-            text-color="white"
-            size="16px"
-            @click="goToEdit"
-            icon="edit"
-          />
+          <div>
+            <q-icon
+              name="edit" size="2em" style="cursor : pointer;" color="white"
+              @click="goToEdit"
+            />
+          </div>
+        </div>
+
+        <div
+          style="margin-top: 1em;">
+          <q-scroll-area
+            style="width: 100%; height: 400px;">
+            <ul style="list-style-type: none;">
+              <li class="list-item">
+                <p
+                  v-for="questionId in currentQuiz.questions"
+                  :key="questionId"
+                  style="font-size: 1.5em; color:black; margin-bottom: 1em;">
+                  {{ $store.getters['quizzes/getQuestionTitleById'](questionId) }}
+                </p>
+              </li>
+            </ul>
+          </q-scroll-area>
         </div>
       </div>
 
-      <div
-        style="margin-top: 1em;">
-        <q-scroll-area
-          style="width: 100%; height: 400px;">
-          <ul style="list-style-type: none;">
-            <li class="list-item">
-              <p
-                v-for="questionId in currentQuiz.questions"
-                :key="questionId"
-                style="font-size: 1.5em; color:black; margin-bottom: 1em;">
-                {{ $store.getters['quizzes/getQuestionTitleById'](questionId) }}
-              </p>
-            </li>
-          </ul>
-        </q-scroll-area>
+      <div class="q-mt-md">
+        <div class="q-pa-md theme-bubble">
+          <q-btn
+            v-if="!currentQuiz.active"
+            unelevated
+            rounded
+            color="white"
+            text-color="black"
+            label="Get link"
+            @click="$store.commit('quizzes/activateQuiz', currentQuiz.id)"
+          />
+        </div>
+
+        <div class="row justify-between" v-if="currentQuiz.active">
+
+          <div>
+            <q-icon
+              @click="showQrcode=true"
+              name="fas fa-qrcode"
+              size="2.5em"
+              style="cursor : pointer;"
+              color="white">
+                <q-tooltip
+                  content-class="bg-white"
+                  content-style="font-size: 1em; color: black; ">
+                  show QR
+                </q-tooltip>
+            </q-icon>
+
+            <q-icon
+              class="q-ml-lg"
+              @click="copyUrl"
+              name="far fa-copy"
+              size="2.5em"
+              style="cursor : pointer;"
+              color="white">
+                <q-tooltip
+                  content-class="bg-white"
+                  content-style="font-size: 1em; color: black; ">
+                  Copy link
+                </q-tooltip>
+            </q-icon>
+
+          </div>
+
+          <div>
+            <q-icon
+              class="q-mr-lg"
+              name="fas fa-trophy"
+              size="2.5em"
+              style="cursor : pointer;"
+              color="white">
+              <q-tooltip
+                content-class="bg-white"
+                content-style="font-size: 1em; color: black; ">
+                Quiz results
+              </q-tooltip>
+            </q-icon>
+
+            <q-btn
+              unelevated
+              rounded
+              class="col-offset-2 col-2"
+              color="white"
+              text-color="black"
+              label="Start quiz"/>
+
+          </div>
+        </div>
       </div>
-
-
     </div>
-
-    <div class="theme-bubble q-mr-lg q-mb-md">
-      <q-btn
-        v-if="!currentQuiz.active"
-        unelevated
-        rounded
-        color="white"
-        style="padding: 0.4em;"
-        text-color="black"
-        label="Get link"
-        @click="$store.commit('quizzes/activateQuiz', currentQuiz.id)"
-      />
-    </div>
-
-    <div class="q-mt-lg" v-if="currentQuiz.active" style="display:flex;">
-
-      <div style="display:inline-grid; width: 100%;">
-        <q-btn
-          class="col"
-          unelevated
-          rounded
-          color="white"
-          style="width: fit-content"
-          text-color="black"
-          :label="getQuizLink"
-          @click="copyLink"/>
-
-        <q-icon v-if="currentQuiz.active" @click="copyUrl" name="far fa-copy" class="q-mt-md " size="2em"
-                style="cursor : pointer;" color="white"/>
-      </div>
-
-      <div
-        class="theme-bubble q-mr-lg q-mb-md">
-
-        <q-btn
-          v-if="currentQuiz.active"
-          unelevated
-          rounded
-          color="white"
-          style="padding: 0.3em;"
-          text-color="black"
-          label="Start quiz"/>
-      </div>
-
-    </div>
-
 
   </section>
 </template>
@@ -127,23 +146,23 @@
       goToEdit() {
         this.$router.push(`quizzes/${this.currentQuiz.id}/questions`);
       },
+
       editQuiz() {
         this.$router.push(`quizzes/${this.currentQuiz.id}`);
       },
-      copyLink() {
-        this.showQrcode = true;
-      },
+
       copyUrl() {
         copyToClipboard(this.getQuizLink);
         this.triggerNotification();
       },
+
       triggerNotification() {
         this.$q.notify({
           type: 'positive',
           message: `Copied link!`,
           actions: [
             {
-              label: 'Dismiss', color: 'white', handler: () => { /* ... */
+              label: 'Dismiss', color: 'white', handler: () => {
               }
             }
           ]
@@ -171,6 +190,10 @@
   .theme-bubble {
     display: flex;
     justify-content: flex-end;
+  }
+
+  .reduce {
+    margin-bottom: 0.5em !important;
   }
 
 </style>
