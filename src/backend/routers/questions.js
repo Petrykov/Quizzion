3bb85126ac1abc0ff2e145b53c9a99b7ae57b8ef
Mark Kravchuk <460888@student.saxion.nl>
2020-06-04 11:21:52 +0200

    Added template for backend

--- src/backend/routers/questions.js
+++ src/backend/routers/questions.js
@@ -1,7 +1,7 @@
 let router = module.exports = require('express').Router();
 const axios = require('axios').default;
 
-axios.defaults.headers.common['X-CSRFToken'] = 'b2228a5703ffdcab1459672d2da782db17b95e916ca45b2ad71a2300dbc3e13bf0e43a7a5ed9f0921bc73a4be4f1478a512c7dfe2753966cba2188300778544f';
+axios.defaults.headers.common['X-CSRFToken'] = 'f2a219ca242cfe271f394fccc59382291bc5a0c581024f5d8d0af5973df969927839af7c9498ee1a72f7ee69a66788fea0fd9ee97e41a8ccd00f6e30a4de5b9c';
 axios.defaults.headers.common['X-Database'] = 'lab';
 axios.defaults.headers.common['Content-Type'] = 'application/json';
 
@@ -31,23 +31,55 @@ router.post('/quizzes/:quiz_id/question', (req, rsp) => {
     // * question name
     // * quiz ID
 
+    if (req.body.name === undefined) {
+        rsp.status(400).json({error: 'The name of question was not provided'});
+    }
+
     console.log("To be sent to server: " + JSON.stringify({
         name: req.body.name,
         label: req.params.quiz_id
     }));
 
     axios.post(getBaseURL() + '/v5/varoption', {
-            name: req.body.name,
-            label: req.params.quiz_id
+        name: req.body.name,
+        label: req.params.quiz_id
     }).then((response) => {
         rsp.status(201).json(response.data);
     }).catch((err) => {
         console.log("error");
-        rsp.status(500).json(err);
-        // throw err;
+        rsp.status(400).json(err);
     });
 });
 
+router.put('/quizzes/:quiz_id/question/:question_id', (req, rsp) => {
+
+    let name = req.body.name;
+
+    if (name === undefined) {
+        rsp.status(400).json({error: 'The name in body was not included'});
+        return;
+    }
+
+    axios.put(getBaseURL() + "/v5/varoption/" + req.params.question_id, {
+        name: name,
+        label: "something"
+    }).then((response) => {
+        rsp.status(200).json(response);
+    }).catch((err) => {
+        console.log("Error with PUT of question");
+        rsp.status(500).json(err);
+    });
+
+});
+
+router.delete('/quizzes/:quiz_id/question/:question_id', (req, rsp) => {
+    axios.delete(getBaseURL() + '/v5/varoption/' + req.params.question_id)
+        .then((response) => {
+            rsp.status(200).json(response);
+        }).catch((err) => {
+            rsp.status(200).json(err);
+    });
+});
 
 function getBaseURL() {
     return 'https://lab.dev.easion.nl/backend/api';
