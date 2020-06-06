a2e2d526ea832865f838007cc142a72b2bd7b35e
Vladyslav <461157@student.saxion.nl>
2020-06-06 14:32:56 +0200

    responsive add quiz question page

--- src/quizzion/src/pages/AddQuizQuestions.vue
+++ src/quizzion/src/pages/AddQuizQuestions.vue
@@ -2,222 +2,236 @@
   <q-page
     class="shadow-2 rounded-borders">
 
-    <q-dialog v-model="alert">
-      <q-card>
-        <q-card-section>
-          <div class="text-h6">Delete?</div>
-        </q-card-section>
+    <div class="logo">
+      <q-dialog v-model="alert">
+        <q-card>
+          <q-card-section>
+            <div class="text-h6">Delete?</div>
+          </q-card-section>
 
-        <q-card-section class="q-pt-none">Are you sure you want to delete this question?</q-card-section>
-        <q-card-actions align="right">
-          <q-btn flat label="OK" color="primary" v-close-popup @click="deleteQuestion"/>
-        </q-card-actions>
+          <q-card-section class="q-pt-none">Are you sure you want to delete this question?</q-card-section>
+          <q-card-actions align="right">
+            <q-btn flat label="OK" color="primary" v-close-popup @click="deleteQuestion"/>
+          </q-card-actions>
 
-      </q-card>
-    </q-dialog>
+        </q-card>
+      </q-dialog>
 
-    <div class="row q-pa-lg logo window-height">
+      <div class="row q-pa-lg  window-height wrapper">
 
-      <q-page-container
-        v-if="currentQuiz"
-        class="col q-pa-xl"
-        style="text-align: center;">
+        <div
+          v-if="currentQuiz"
+          class="col q-pa-xl"
+          style="text-align: center;justify-content: center; align-items: center; display: flex;">
 
-        <p
-          class="q-mt-lg"
-          style="font-size:3em;">{{ currentQuiz.title }}</p>
+          <div class="question-list-wrapper">
+            <p
+              class="q-mt-lg"
+              style="font-size:3em;">{{ currentQuiz.title }}</p>
 
-        <div class="q-mt-xl vertical-allignment">
+            <div class="q-mt-xl">
 
-          <q-scroll-area
-            class="scroll-area"
-            style="height: 200px; max-width: 300px;">
+              <q-scroll-area
+                class="scroll-area"
+                style="height: 200px; max-width: 300px;">
 
-            <div>
-              <p
-                v-for="(questionId) in currentQuiz.questions"
-                :key="questionId">
+                <div>
+                  <p
+                    v-for="(questionId) in currentQuiz.questions"
+                    :key="questionId">
 
-                <q-btn
-                  class="q-mt-lg"
-                  style="width:90%;"
-                  :outline="questionId != selectedQuestionId"
-                  rounded
-                  color="black"
-                  @click="onQuestionClick(questionId)">
-                  {{ questionTitle(questionId) }}
-                </q-btn>
+                    <q-btn
+                      class="q-mt-lg"
+                      style="width:90%;"
+                      :outline="questionId !== selectedQuestionId"
+                      rounded
+                      color="black"
+                      @click="onQuestionClick(questionId)">
+                      {{ questionTitle(questionId) }}
+                    </q-btn>
 
-              </p>
+                  </p>
+                </div>
+              </q-scroll-area>
+
+              <q-icon
+                name="add_circle_outline"
+                color="green-7"
+                style="cursor : pointer;"
+                size="3em"
+                class="q-mt-md"
+                @click="addQuestion"/>
+
+
             </div>
-          </q-scroll-area>
-
-          <q-icon
-            name="add_circle_outline"
-            color="green-7"
-            style="cursor : pointer;"
-            size="3em"
-            class="q-mt-md"
-            @click="addQuestion"/>
-
+          </div>
 
         </div>
-      </q-page-container>
 
-      <div
-        class="col"
-        :style="{background: currentQuiz.color}"
-        style="border-radius: 2em; box-shadow: 10px 10px 30px rgba(0, 0.5, 0.5, 0.5);"
-        v-if="question !== ' '">
+        <div
+          class="col question-card-wrapper"
+          :style="{background: currentQuiz.color}"
+          style="border-radius: 2em; box-shadow: 10px 10px 30px rgba(0, 0.5, 0.5, 0.5);"
+          v-if="question !== ' '">
 
-        <q-page padding>
+          <q-page padding>
 
-          <div style="display:flex;">
-
-            <div
-              style="width: 100%;">
-              <q-input
-                style="font-size: 1.8em;"
-                dark
-                color="grey-12"
-                label="Question's title"
-                v-model="question.title"
-                label-color="grey"/>
-            </div>
-
-            <div>
-              <q-icon
-                name="delete"
-                color="white"
-                style="cursor : pointer; position: absolute; right: 0;"
-                class="q-mr-md"
-                size="3em"
-                @click="promptToDelete"/>
-            </div>
-
-          </div>
-
-          <q-input
-            dark
-            filled
-            autogrow
-            label="Question description"
-            v-model="question.description"
-            class="q-mt-md"
-            color="grey"/>
-
-          <p
-            class="paragraph q-mt-lg q-ml-md"
-            style="color:white; font-size:2em;">
-            The answers?
-          </p>
-
-          <q-scroll-area
-            class="scroll-area scrollarea"
-            style="height: 275px; max-width: 300px;">
-
-            <div
-              class="col q-mt-sm">
+            <div style="display:flex;">
 
               <div
-                v-for="(answer, index) in answersList"
-                :key="index"
-                class="row q-mt-xs">
-
-                <q-checkbox
-                  class="q-mt-sm q-mr-xs"
-                  v-model="answer.correct"
-                  dark/>
-
+                style="width: 100%;">
                 <q-input
-                  dense
-                  style="color : grey;"
-                  v-model="answer.label"
-                  dark/>
+                  style="font-size: 1.8em;"
+                  dark
+                  color="grey-12"
+                  label="Question's title"
+                  v-model="question.title"
+                  label-color="grey"/>
+              </div>
 
+              <div>
                 <q-icon
-                  name="clear"
+                  name="delete"
                   color="white"
-                  class="q-mt-md q-ml-sm"
-                  style="cursor : pointer;"
-                  size="2em"
-                  @click="deleteAnswer(answer.id)"/>
-
+                  style="cursor : pointer; position: absolute; right: 0;"
+                  class="q-mr-md"
+                  size="3em"
+                  @click="promptToDelete"/>
               </div>
 
-              <form>
-                <div
-                  class="row q-mt-md q-ml-md">
+            </div>
 
-                  <q-input
-                    class="q-ml-lg"
-                    style="color:grey;"
-                    label="Add new answer"
-                    v-model="newAnswer"
-                    :rules="[val => !!val || 'Field is required']"
-                    dark/>
+            <q-input
+              dark
+              filled
+              autogrow
+              label="Question description"
+              v-model="question.description"
+              class="q-mt-md"
+              color="grey"/>
 
-                  <q-icon
-                    v-if="newAnswer.length !== 0"
-                    name="add_circle_outline"
-                    color="white"
-                    class="q-mt-md q-ml-xs"
-                    style="cursor : pointer;"
-                    size="2em"
-                    @click="addAnswer"/>
+            <div class="answers-div-wrapper">
+              <p
+                class="paragraph q-mt-lg q-ml-md"
+                style="color:white; font-size:2em;">
+                The answers?
+              </p>
 
+              <div>
+                <q-scroll-area
+                  class="scroll-area scrollarea answers-wrapper"
+                  style="height: 275px; max-width: 300px;">
+
+                  <div
+                    class="col q-mt-sm">
+
+                    <div
+                      v-for="(answer, index) in answersList"
+                      :key="index"
+                      class="row q-mt-xs">
+
+                      <q-checkbox
+                        class="q-mt-sm q-mr-xs"
+                        v-model="answer.correct"
+                        dark/>
+
+                      <q-input
+                        dense
+                        style="color : grey;"
+                        v-model="answer.label"
+                        dark/>
+
+                      <q-icon
+                        name="clear"
+                        color="white"
+                        class="q-mt-md q-ml-sm"
+                        style="cursor : pointer;"
+                        size="2em"
+                        @click="deleteAnswer(answer.id)"/>
+
+                    </div>
+
+                    <form>
+                      <div
+                        class="row q-mt-md q-ml-md">
+
+                        <q-input
+                          class="q-ml-lg"
+                          style="color:grey;"
+                          label="Add new answer"
+                          v-model="newAnswer"
+                          :rules="[val => !!val || 'Field is required']"
+                          dark/>
+
+                        <q-icon
+                          v-if="newAnswer.length !== 0"
+                          name="add_circle_outline"
+                          color="white"
+                          class="q-mt-md q-ml-xs"
+                          style="cursor : pointer;"
+                          size="2em"
+                          @click="addAnswer"/>
+
+                      </div>
+                    </form>
+                  </div>
+
+                </q-scroll-area>
+              </div>
+
+            </div>
+
+            <div class="timer-wrapper">
+              <p
+                class="q-mt-md"
+                style="color:white; font-size:2em;">What about timer?</p>
+
+              <q-icon
+                name="timer"
+                color="white"
+                size="5em"
+                class="q-mr-xs q-mt-md time-icon"/>
+
+              <div
+                class="timer">
+
+                <div class="row col">
+                  <div class="q-pa-md q-gutter-sm time-section">
+                    <q-btn
+                      v-for="(time, index) in ['5 sec', '10 sec', '15 sec', '30 sec', '1 min']"
+                      v-bind:key="index"
+                      :id="selectedQuestion.id + '=' + index"
+                      round
+                      style="border: 1px solid black; padding: 1em;"
+                      color="white"
+                      text-color="black"
+                      class="time-button"
+                      :class="{selected : (parseInt(time.split(' ')[0], 10) === question.time)}"
+                      :label="time"
+                      @click="onTimeClick(time, index)"/>
+                  </div>
                 </div>
-              </form>
-            </div>
-
-          </q-scroll-area>
-
-          <p
-            class="q-mt-md"
-            style="color:white; font-size:2em;">What about timer?</p>
-
-          <div
-            class="row">
-
-            <q-icon
-              name="timer"
-              color="white"
-              size="5em"
-              class="q-mr-xs q-mt-md"/>
-
-            <div class="row col">
-              <div class="q-pa-md q-gutter-sm">
-                <q-btn
-                  v-for="(time, index) in ['5 sec', '10 sec', '15 sec', '30 sec', '1 min']"
-                  v-bind:key="index"
-                  :id="selectedQuestion.id + '=' + index"
-                  round
-                  style="border: 1px solid black; padding: 1em;"
-                  color="white"
-                  text-color="black"
-                  class="timer"
-                  :class="{selected : (parseInt(time.split(' ')[0], 10) === question.time)}"
-                  :label="time"
-                  @click="onTimeClick(time, index)"/>
               </div>
             </div>
-          </div>
 
-          <div
-            class="q-mt-md"
-            style="width: 100%; text-align: center;">
 
-            <q-btn
-              label="save"
-                            color="white"
-              text-color="black"
-              class="q-ml-md"
-              style="cursor : pointer; padding: 0.2em 0.4em; border-radius: 2em;"
-              size="1.2em"
-              @click="updateQuestion"/>
-          </div>
+            <div
+              class="q-mt-md"
+              style="width: 100%; text-align: center;">
 
-        </q-page>
+              <q-btn
+                label="save"
+                color="white"
+                text-color="black"
+                class="q-ml-md"
+                style="cursor : pointer; padding: 0.2em 0.4em; border-radius: 2em;"
+                size="1.2em"
+                @click="updateQuestion"/>
+            </div>
+
+          </q-page>
+        </div>
+
       </div>
     </div>
   </q-page>
@@ -434,7 +448,7 @@
   }
 
   .logo {
-    display: flex;
+    /*display: flex;*/
     background-image: url("~assets/bg_answer_screen.png");
     border: 1px solid black;
     height: 100%;
@@ -455,6 +469,76 @@
     background: #5dbcd2 !important;
     border: 1px solid black !important;
     color: black !important;
+  }
+
+  .timer {
+    display: flex;
+  }
+
+  .time-section {
+    width: 100%;
+    margin: 0 !important;
+  }
+
+  .question-list-wrapper {
+    display: inline-block;
+  }
+
+  .timer-wrapper {
+    text-align: center;
+  }
+
+  @media screen and (max-width: 1200px) {
+
+    .timer {
+      display: inline-block;
+      width: 100%;
+      text-align: center;
+    }
+
+    .time-section {
+      margin: 0 auto;
+    }
+
+    .wrapper {
+      display: inline-block;
+      width: 100%;
+    }
+
+    .question-card-wrapper {
+      width: 70%;
+      margin: 0 auto;
+    }
+
+    .timer-wrapper {
+      text-align: center;
+    }
+  }
+
+  @media screen and (max-width: 600px) {
+    .question-card-wrapper {
+      width: 90%;
+    }
+
+    .timer-wrapper {
+      display: grid;
+      text-align: center;
+    }
+
+    .timer{
+      width: 80%;
+      margin: 0 auto;
+    }
+
+    .time-icon{
+      margin: 0 auto;
+    }
+
+    .wrapper{
+      height: 50%;
+      padding: 0 !important;
+      margin: 0 !important;
+    }
   }
 
 </style>
