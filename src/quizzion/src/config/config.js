7ae496fcc61d585a5a3050c228387d72828af1ab
Mark Kravchuk <460888@student.saxion.nl>
2020-06-21 19:25:09 +0200

    Fixed bugs a bit

--- src/quizzion/src/config/config.js
+++ src/quizzion/src/config/config.js
@@ -1,6 +1,6 @@
 const config = {
-  frontendPath: 'http://192.168.2.15:8080/#',
-  backendPath: 'http://192.168.2.15:3000'
+  frontendPath: 'http://localhost:8080/#',
+  backendPath: 'http://localhost:3000'
 }
 
 module.exports = config;
